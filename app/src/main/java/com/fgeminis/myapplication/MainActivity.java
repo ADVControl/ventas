024ad4060e948f4fff6dd2b65ccc9c4ec129a9c2
Guaaan <72053761+Guaaan@users.noreply.github.com>
2021-05-06 15:22:33 -0400

    se cierra al presionar add

--- app/src/main/java/com/fgeminis/myapplication/MainActivity.java
+++ app/src/main/java/com/fgeminis/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -37,10 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         boton = findViewById(R.id.boton);
 
-        boton.OnClickListener(new View.OnClickListener() {
+        boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                add();
             }
         });
     }
@@ -48,10 +50,55 @@ public class MainActivity extends AppCompatActivity {
     public void add()
     {
         int tot;
-        String = ed1.getText().toString();
+        String nombreProd = ed1.getText().toString();
         int precio = Integer.parseInt(ed2.getText().toString());
         int cantidad = Integer.parseInt(ed3.getText().toString());
         tot = precio * cantidad;
+
+        data.add(nombreProd);
+        data1.add(String.valueOf(precio));
+        data2.add(String.valueOf(cantidad));
+        data3.add(String.valueOf(tot));
+
+        TableRow row = new TableRow(this);
+        TextView t1 = new TextView(this);
+        TextView t2 = new TextView(this);
+        TextView t3 = new TextView(this);
+        TextView t4 = new TextView(this);
+
+        String total;
+
+        int sum = 0;
+
+        for(int i = 0; i< data.size(); i++)
+        {
+            String nombrep = data.get(i);
+            String prd = data1.get(i);
+            String ctd = data2.get(i);
+            total = data3.get(i);
+
+            t1.setText(nombrep);
+            t2.setText(prd);
+            t3.setText(ctd);
+            t4.setText(total);
+
+            sum = sum + Integer.parseInt(data3.get(i).toString());
+
+
+        }
+
+        row.addView(t1);
+        row.addView(t2);
+        row.addView(t3);
+        row.addView(t4);
+        table.addView(row);
+
+
+ed4.setText(String.valueOf(sum));
+ed1.setText("");
+ed2.setText("");
+ed3.setText("");
+ed1.requestFocus();
     }
 
 
